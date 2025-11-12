946e517e563b5756d7aec953027295e291da8948
Maarten Hulsman <maarten.hulsman@outlook.com>
2025-11-12 17:16:36 +0100

    beginning of the structure

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,10 +1,28 @@
 import pathlib
 import geopandas
+import pandas as pd
 
 
-def dataloader(filepath: str) -> geopandas:
+def dataloader() -> geopandas:
+    """'
+    In this function all the filepaths are defined. Only the most important columns are saved.
+    """
     script_dir = pathlib.Path(__file__).parent
-    path_to_data = script_dir.parent / filepath
-    gdf = geopandas.read_file(path_to_data)
+    filepaths = {
+        "Delft": "delft_trees.geojson",
+        # "The Hague": "the_hague_trees.csv" # https://data.overheid.nl/dataset/bomen-csv#metadata,
+        # NOTE the data of The Hague doesn't look correct when I compare it with:
+        # https://experience.arcgis.com/experience/60100b5ce8444943a7ff4fd8f577f5d1/?org=DDH#data_s=id%3A6cb9371f18584708b01723d9c72714c2-Natuur_en_landschapsbeheer_5488%3A1621
+    }
+    tree_gdfs = []
+    for city in filepaths.keys():
+        tree_gdf = geopandas.read_file(script_dir.parent / "data" / filepaths[city])[
+            ["BUURT", "WIJK"]
+        ]
+        tree_gdf["GEMEENTE"] = city
+        tree_gdf[["GEMEENTE", "BUURT", "WIJK"]]
+        tree_gdfs.append(tree_gdf)
 
-    return gdf
+    return pd.concat(
+        tree_gdfs, ignore_index=True
+    )  # TODO: when using multiple datasets make sure all columns have the same meaning
--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from dataloader import dataloader
+from tree_density import tree_density
 
 
 def main():
-    delft_tree_data_path = "data/delft_trees.geojson"
-    delft_tree_data = dataloader(delft_tree_data_path)
-    print(delft_tree_data)
+    tree_data = dataloader()
+    tree_density(tree_data)
 
 
 if __name__ == "__main__":

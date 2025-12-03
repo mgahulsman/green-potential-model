37180a70769a482cf40d045171285f9e85566b65
Maarten Hulsman <maarten.hulsman@outlook.com>
2025-12-03 21:17:38 +0100

    added visualizer to main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from dataloaders.municipality_trees import municipality_tree_loader
 from tree_density import tree_density
+from visualizer import visualize_data
 
 
 def main():
     tree_data = municipality_tree_loader()
     tree_density(tree_data)
+    visualize_data()
 
 
 if __name__ == "__main__":
--- src/visualizer.py
+++ src/visualizer.py
@@ -1,33 +1,42 @@
+from pathlib import Path
+import webbrowser
 import geopandas as gpd
 import folium
 
 
-edges_gdf = gpd.read_file("data/roads/delft_edges.geojson")
-test_gdf = gpd.read_file("data/test.geojson")
+def visualize_data(page_name="index.html", open_browser=True):
+    edges_gdf = gpd.read_file("data/roads/delft_edges.geojson")
+    test_gdf = gpd.read_file("data/test.geojson")
 
-# NOTE: Optioneel: Herprojecteer naar WGS 84 (EPSG:4326),
-# de standaard voor webkaarten, als uw data dit nog niet is.
-# Dit zorgt voor compatibiliteit met Folium/Leaflet.
-if edges_gdf.crs and edges_gdf.crs.to_epsg() != 4326:
-    edges_gdf = edges_gdf.to_crs(epsg=4326)
+    # NOTE: Optioneel: Herprojecteer naar WGS 84 (EPSG:4326),
+    # de standaard voor webkaarten, als uw data dit nog niet is.
+    # Dit zorgt voor compatibiliteit met Folium/Leaflet.
+    if edges_gdf.crs and edges_gdf.crs.to_epsg() != 4326:
+        edges_gdf = edges_gdf.to_crs(epsg=4326)
 
-center = edges_gdf.geometry.union_all().centroid
-m = folium.Map(location=[center.y, center.x], zoom_start=12)
+    center = edges_gdf.geometry.union_all().centroid
+    m = folium.Map(location=[center.y, center.x], zoom_start=12)
 
-folium.GeoJson(
-    edges_gdf.to_json(),
-    name="Wegennetwerk",
-    style_function=lambda x: {"color": "red", "weight": 2, "opacity": 0.7},
-).add_to(m)
+    folium.GeoJson(
+        edges_gdf.to_json(),
+        name="Wegennetwerk",
+        style_function=lambda x: {"color": "red", "weight": 2, "opacity": 0.7},
+    ).add_to(m)
 
-folium.GeoJson(
-    test_gdf.to_json(),
-    name="test",
-    style_function=lambda x: {"color": "blue", "weight": 2, "opacity": 0.7},
-    tooltip=folium.features.GeoJsonTooltip(
-        fields=["Naam"],
-        aliases=["Pt:"],
-    ),
-).add_to(m)
+    folium.GeoJson(
+        test_gdf.to_json(),
+        name="test",
+        style_function=lambda x: {"color": "blue", "weight": 2, "opacity": 0.7},
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=["Naam"],
+            aliases=["Testttt:"],
+        ),
+    ).add_to(m)
 
-m.save("index.html")
+    m.save(page_name)
+
+    if open_browser:
+        dir_path = Path.cwd()
+        index_file = page_name
+        full_path = dir_path / index_file
+        webbrowser.open(str(full_path))

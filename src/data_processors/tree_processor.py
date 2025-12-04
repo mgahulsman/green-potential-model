0763b4e5c91bc8fb1b0dc9cd5b79e5f8f6ad66fe
Maarten Hulsman <maarten.hulsman@outlook.com>
2025-12-04 10:21:41 +0100

    Created the skelet for the rest of the system

--- src/data_processors/tree_processor.py
+++ src/data_processors/tree_processor.py
@@ -1,30 +1,40 @@
 from pathlib import Path
-import geopandas
+import geopandas as gpd
 import pandas as pd
 
 
-def tree_processor() -> None:
-    """'
-    In this function all the tree filepaths are defined. A restriction area is added by adding circle
-    """
-    # NOTE: this function is build in such a way that more municipalities can be added
+def process_tree_data() -> Path:
     dir_path = Path.cwd()
-    data_folder = "data"
-    raw_folder = "raw"
-    tree_folder = "trees"
-    processed_folder = "processed"
-    raw_path = dir_path / data_folder / raw_folder / tree_folder
-    processed_path = dir_path / data_folder / processed_folder
+    raw_path = dir_path / "data" / "raw" / "trees"
+    processed_path = dir_path / "data" / "processed"
+    output_file = processed_path / "merged_tree_area.geojson"
 
-    filepaths = {
-        "Delft": "delft_trees.geojson",
-    }
+    filepaths = {"Delft": "delft_trees.geojson"}
     tree_gdfs = []
+
     for city in filepaths.keys():
-        tree_gdf = geopandas.read_file(raw_path / filepaths[city])[["geometry"]]
-        # TODO: Create a circel
+        tree_gdf = gpd.read_file(raw_path / filepaths[city])
+        # binnen_stad_bomen = tree_gdf.loc[tree_gdf["WIJK"] == "11 Binnenstad"]
         tree_gdfs.append(tree_gdf)
 
-    tree_gdf = pd.concat(tree_gdfs, ignore_index=True)
+    final_gdf = pd.concat(tree_gdfs, ignore_index=True)
 
-    tree_gdf.to_file(processed_path / "trees.geojson", driver="GeoJSON")
+    UTM_CRS = "EPSG:32631"
+    gdf_geprojecteerd = final_gdf.to_crs(UTM_CRS)
+
+    geometrie_cirkels = gdf_geprojecteerd["geometry"].buffer(5, resolution=16)
+
+    merged_polygon = geometrie_cirkels.unary_union
+
+    merged_gs = gpd.GeoSeries([merged_polygon], crs=UTM_CRS)
+    merged_gs_wgs84 = merged_gs.to_crs(epsg=4326)
+
+    gdf_final_merged = gpd.GeoDataFrame(
+        data={"id": [1], "beschrijving": ["Gezamenlijk buffergebied"]},
+        geometry=merged_gs_wgs84,
+        crs=merged_gs_wgs84.crs,
+    )
+
+    gdf_final_merged.to_file(output_file, driver="GeoJSON")
+
+    return output_file
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
-from data_processors.tree_processor import tree_processor
+from data_processors.tree_processor import process_tree_data
+from visualizer import visualize_processed_data
 
 
 def main():
     # tree_data = municipality_tree_loader()
     # tree_density(tree_data)
-    tree_processor()
-    # visualize_data()
+    processed_file = process_tree_data()
+    visualize_processed_data(processed_file)
 
 
 if __name__ == "__main__":
